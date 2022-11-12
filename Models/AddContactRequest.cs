7a87586b4e750e942ecc824700742a39fe9bf658
oluseyi <oluseyi@tm30.net>
2022-11-12 08:27:40 +0100

    first commit

---
+++ Models/AddContactRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsAPI.Models
+{
+    public class AddContactRequest
+    {
+        public string FullName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        [MinLength(10, ErrorMessage = "Address should be minimum of 10 characters")]
+        [MaxLength(200, ErrorMessage = "Address should be maximum of 200 characters")]
+        public string Address { get; set; }
+    }
+}
